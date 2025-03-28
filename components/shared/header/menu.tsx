5ef042bfc51b7a2199af3d30b54d60e7e41b6877
Hubert Quarshie <hugheyq@gmail.com>
2025-03-28 14:23:54 +0000

    Added Sign Out functionality

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,36 +1,24 @@
 import { Button } from '@/components/ui/button'
 import { ThemeToggle } from './theme-toggle'
 import Link from 'next/link'
-import {
-	EllipsisVertical,
-	ShoppingCart,
-	UserIcon,
-} from 'lucide-react'
-import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
-} from '@/components/ui/sheet'
+import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import UserButton from './user-button'
 
 function Menu() {
 	return (
 		<div className='flex justify-end gap-3'>
 			<nav className='hidden md:flex w-full max-w-xs gap-1'>
 				<ThemeToggle variant='ghost' />
-				<Button asChild variant={'ghost'}>
+				<Button
+					asChild
+					variant={'ghost'}
+				>
 					<Link href={'/cart'}>
 						<ShoppingCart />
 					</Link>
 				</Button>
-				<Button asChild>
-					<Link href={'/sign-in'}>
-						Sign In
-						{/* <UserIcon /> */}
-					</Link>
-				</Button>
+				<UserButton />
 			</nav>
 
 			<nav className='md:hidden'>
@@ -48,16 +36,15 @@ function Menu() {
 						<div className='px-4'>
 							<div className='flex flex-col gap-3'>
 								<ThemeToggle variant='outline' />
-								<Button asChild variant={'outline'}>
+								<Button
+									asChild
+									variant={'outline'}
+								>
 									<Link href={'/cart'}>
 										<ShoppingCart /> Cart
 									</Link>
 								</Button>
-								<Button asChild>
-									<Link href={'/sign-in'}>
-										<UserIcon /> Sign in
-									</Link>
-								</Button>
+								<UserButton />
 							</div>
 						</div>
 					</SheetContent>
