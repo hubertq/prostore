57a5df780777a51fa65be22a2264ef711b8d2872
Hubert Quarshie <hugheyq@gmail.com>
2025-03-28 15:15:07 +0000

    Completed sign up functionality and customized token

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signInFormSchema, signUpFormSchema } from '../validators'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { hashSync } from 'bcrypt-ts-edge'
 import { prisma } from '@/db/prisma'
+import { formatError } from '../utils'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -57,6 +58,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 		return { success: true, message: 'User registered successfully.' }
 	} catch (error) {
 		if (isRedirectError(error)) throw error
-		return { success: false, message: 'User was not registered' }
+		return { success: false, message: formatError(error) }
 	}
 }
