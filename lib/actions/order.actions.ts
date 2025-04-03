f862882b34f8c0f4fafd5e3a225652f5bdb39aaa
Hubert Quarshie <huberts.quarshie@gmail.com>
2025-04-03 19:27:02 +0000

    Resolved es-lint error

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -151,35 +151,35 @@ export async function approvePayPalOrder(orderId: string, data: { orderID: strin
 }
 
 // Update order to paid
-async function updateOrderToPaid({ orderId, paymentResult }: { orderId: string; paymentResult?: PaymentResult }) {
-	// Get order from database
-	const order = await prisma.order.findFirst({
-		where: {
-			id: orderId,
-		},
-		include: {
-			orderitems: true,
-		},
-	})
+// async function updateOrderToPaid({ orderId, paymentResult }: { orderId: string; paymentResult?: PaymentResult }) {
+// 	// Get order from database
+// 	const order = await prisma.order.findFirst({
+// 		where: {
+// 			id: orderId,
+// 		},
+// 		include: {
+// 			orderitems: true,
+// 		},
+// 	})
 
-	if (!order) throw new Error('Order not found')
+// 	if (!order) throw new Error('Order not found')
 
-	if (order.isPaid) throw new Error('Order is already paid')
+// 	if (order.isPaid) throw new Error('Order is already paid')
 
-	// Transaction to update order and account for product stock
-	await prisma.$transaction(async tx => {
-		// Iterate over products and update stock
-		for (const item of order.orderitems) {
-			await tx.product.update({
-				where: { id: item.productId },
-				data: { stock: { increment: -item.qty } },
-			})
-		}
+// 	// Transaction to update order and account for product stock
+// 	await prisma.$transaction(async tx => {
+// 		// Iterate over products and update stock
+// 		for (const item of order.orderitems) {
+// 			await tx.product.update({
+// 				where: { id: item.productId },
+// 				data: { stock: { increment: -item.qty } },
+// 			})
+// 		}
 
-		// Set the order to paid
-		await tx.order.update({
-			where: { id: orderId },
-			data: { isPaid: true, paidAt: new Date(), paymentResult },
-		})
-	})
-}
+// 		// Set the order to paid
+// 		await tx.order.update({
+// 			where: { id: orderId },
+// 			data: { isPaid: true, paidAt: new Date(), paymentResult },
+// 		})
+// 	})
+// }
